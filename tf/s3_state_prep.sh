79ee417c4472e4680add7a1c453715e36135dac6
Justin Bankes <12632465+jbankes@users.noreply.github.com>
2024-03-17 18:24:31 +0000

    feat: add new terraform for ec2 experiement

--- tf/s3_state_prep.sh
+++ tf/s3_state_prep.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# This script prepares the state for the S3 state backend.
+# This script prepares the state for the S3 state backend and exports a main.tf
+# file with the backend configuration.
 
 # Get name and region from user
-read -p "Enter a prefix for the S3 bucket to store Terraform state: " TF_PREFIX
+read -p "Enter a prefix for the project to store Terraform state: " TF_PREFIX
 read -p "Enter the AWS region to create the resources in [us-west-2]: " REGION
 REGION=${REGION:-us-west-2}
 
 # check for aws s3 bucket and create if not there 
-if aws s3api head-bucket --bucket "$TF_PREFIX-terraform-states" 2>/dev/null; then
+if aws s3api head-bucket --bucket "$TF_PREFIX-terraform-states" 2>/dev/null --no-cli-pager; then
   echo "Bucket exists"
 else
   aws s3api create-bucket \
@@ -21,14 +22,10 @@ else
     --versioning-configuration Status=Enabled
 
   echo "$TF_PREFIX-terraform-states bucket created and versioning enabled"
-  
-  # replace BUCKET-PREFIX in main.tf with $TF_PREFIX
-  sed -i "s/BUCKET-PREFIX/$TF_PREFIX/g" main.tf
-  echo "main.tf bucket in backend block updated with $TF_PREFIX"
 fi
 
 # check for dynamodb table and create if not there
-if aws dynamodb describe-table --table-name "$TF_PREFIX-terraform-locks" 2>/dev/null; then
+if aws dynamodb describe-table --table-name "$TF_PREFIX-terraform-locks" 2>/dev/null --no-cli-pager; then
   echo "Table exists"
 else
   aws dynamodb create-table \
@@ -39,9 +36,17 @@ else
     --region $REGION \
     --no-cli-pager
 
-   echo "$TF_PREFIX-terraform-locks table created" 
-
-  # replace DBTABLE-PREFIX in main.tf with $TF_PREFIX
-  sed -i "s/DBTABLE-PREFIX/$TF_PREFIX/g" main.tf
-  echo "main.tf dynamodb_table in backend block updated with $TF_PREFIX"
+  echo "$TF_PREFIX-terraform-locks table created" 
 fi
+
+# create multiline terraform backend in new main.tf
+cat > main.tf << EOF
+terraform {
+  backend "s3" {
+    bucket = "$TF_PREFIX-terraform-states"
+    key    = "terraform.tfstate"
+    region = "$REGION"
+    dynamodb_table = "$TF_PREFIX-terraform-locks"
+  }
+}
+EOF
